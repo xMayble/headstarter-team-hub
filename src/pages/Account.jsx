e5e3acef65c2eb4d8ca18d230779f10888442e88
Rifat Roufe <rzr210@nyu.edu>
2023-02-14 23:37:12 -0500

    User Auth #2 set of changes

---
+++ src/pages/Account.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Account = () => {
+    return (
+        <><div className='w-[300px] m-auto'>
+            <h1 className='text-center text-2xl font-bold pt-12'>Account</h1>
+        </div><p>Welcome, User</p><div>
+                <button className='border py-2 px-5 mt-10'>Logout</button>
+            </div></>
+    );
+};
+
+export default Account;
