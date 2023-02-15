ca1e81ea5d394d9ea80e2fe0a1f1fa9a4773faf4
Rifat Roufe <rzr210@nyu.edu>
2023-02-14 23:08:19 -0500

    User Auth #1 set of changes

--- src/App.js
+++ src/App.js
@@ -2,10 +2,17 @@ import React from 'react';
 import Calendar from './Components/Calendar';
 import logo from './logo.svg';
 import './App.css';
+import { Routes } from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
       <Calendar />
     </div>
   )
