1b18666166fe3ccb7fb0c3936338da8e4ed8dd63
Adib Rahim <adibrahim27@gmail.com>
2023-02-16 15:40:53 -0500

    added styling + css

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import Calendar from './Components/Calendar';
 import logo from './logo.svg';
 import './App.css';
+import VideoCall from './Components/VideoCall';
 
 const App = () => {
   return (
     <div>
       <Calendar />
+      <VideoCall />
     </div>
   )
 };
--- src/Components/videoCall.js
+++ src/Components/videoCall.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import "./VideoCall.css"; // Import CSS file for styling
 
 const VideoCall = () => {
   const [localStream, setLocalStream] = useState(null);
@@ -40,10 +41,15 @@ const VideoCall = () => {
 
   return (
     <div className="video-call">
-      <video src={remoteStream ? URL.createObjectURL(remoteStream) : ""} />
-      <video src={localStream ? URL.createObjectURL(localStream) : ""} />
-      <button onClick={startCall}>Start Call</button>
-      <button onClick={endCall}>End Call</button>
+      <h1>Video Call</h1>
+      <div className="video-container">
+        <video src={remoteStream ? URL.createObjectURL(remoteStream) : ""} />
+        <video src={localStream ? URL.createObjectURL(localStream) : ""} />
+      </div>
+      <div className="button-container">
+        <button className="start-call" onClick={startCall}>Start Call</button>
+        <button className="end-call" onClick={endCall}>End Call</button>
+      </div>
     </div>
   );
 };
